1a97964f5c072a2967bf04d438825e5ca05aa754
Thanh Duy Nguyen <duythanh1565@gmail.com>
2022-11-25 10:31:42 +0700

    update data and plugin

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,6 @@
 <?php
+define( 'WP_CACHE', true );
+
 /**
  * The base configuration for WordPress
  *
@@ -29,7 +31,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'thanhanh' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
